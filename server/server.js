5fab2d13f31b1a71e5fc4611a238ad0d1c3e69c7
Alina Prystynska <pristinskayaalina9@gmail.com>
2026-03-06 12:00:49 +0000

    fix(api): support DB_PORT and hosted MySQL SSL config

--- server/server.js
+++ server/server.js
@@ -51,9 +51,11 @@ const upload = multer({
 
 const db = mysql.createPool({
   host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT) || 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  ssl: process.env.DB_SSL === 'false' ? undefined : { rejectUnauthorized: false },
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
